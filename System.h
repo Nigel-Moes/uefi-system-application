8413c0b3b40d5fd4022377f0dbaa85e188f9b0c4
Nigel Moes <68500902+Nigel-Moes@users.noreply.github.com>
2024-11-06 22:54:43 +0100

    Add files via upload

---
+++ System.h
@@ -0,0 +1,10 @@
+#ifndef _SYSTEM_H__
+#define _SYSTEM_H__
+
+#include <Library/MemoryAllocationLib.h>
+#include <Library/PrintLib.h>
+#include <Library/UefiBootServicesTableLib.h>
+#include <Library/UefiLib.h>
+#include <Uefi.h>
+
+#endif
